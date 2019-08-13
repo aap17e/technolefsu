a3d1883c23fbc530655bcff510ffadf3adb88801
aap17e <aap17e@users.noreply.github.com>
2019-08-12 21:23:37 -0400

    second commit

--- typing.js
+++ typing.js
@@ -1,3 +1,29 @@
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks= document.querySelectorAll('.nav-links li');
+//     //Toggle now
+
+//     burger.addEventListener('click',()=>{
+//         nav.classList.toggle('nav-active');
+
+//             //Animate Links
+//     navLinks.forEach((link, index) => {
+//         if(link.style.animation){
+//             link.style.animation = ''
+//         } else {
+//             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+//         }
+//     });
+//     //burger animation
+//     burger.classList.toggle('toggle');
+//     });
+
+// }
+// navSlide();
+
+
+
 var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
